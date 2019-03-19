5036ff9e65d06595d785198ecb803bc855111f1a
Nick Mitchell <nick@kitset.io>
2019-03-19 14:58:21 +1100

    readme

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,15 +2,26 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
+/**
+*
+* /auth 
+*
+**/
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  /** 
+  * GET /auth/token 
+  **/
   @Get('token')
   async createToken(): Promise<any> {
     return await this.authService.createToken();
   }
 
+  /** 
+  * GET /auth/data 
+  **/
   @Get('data')
   @UseGuards(AuthGuard())
   findAll() {
